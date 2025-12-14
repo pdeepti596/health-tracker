c3de935098251282d27ad20674025490721c4853
pdeepti596 <pdeepti849@gmail.com>
2025-12-14 17:35:23 +0000

    connection complete

--- database/queries.py
+++ database/queries.py
@@ -23,7 +23,59 @@ def db_login_user(email, password):
          SELECT * FROM users WHERE email=? AND password=?
     """, (email, password)).fetchone()
     conn.close()
-    return dict(row) if row else None               
+    return dict(row) if row else None 
+
+def db_get_user(user_id):
+    conn = get_connection
+    row = conn.execute("SELECT * FROM users WHERE id=?", (user_id),).fetchone()
+    conn.close()
+    return dict(row) if row else None
+
+def db_update_user(user_id, data):
+    conn = get_connection()
+    now = datetime.now().isoformat()
+
+    conn.execute("""
+        UPDATE users SET name=?, email=?, password=?, updated_at=?
+        WHERE id=?
+        """, (data["name"], data["email"], data["password"], now, user_id))
+
+        conn.commit()
+        conn.close()
+        return db_get_user(user_id)
+
+def db_delete_user(id):
+    user = db_get_user(user_id)
+    if not user:
+        return None
+
+    conn = get_connection()
+    conn.execute("DELETE FROM users WHERE id=?", (user_id))
+    conn.commit()
+    conn.close()
+    return user
+
+    # user input crud (age, height, weight, gender)
+
+def db_create_user_input(data):
+    conn = get_connection()
+    now = datetime.now().isoformat()
+
+    cur = conn.execute("""
+        INSERT INTO user_input (user_id, age, height, weight, gender, created_at)
+         VALUES (?, ?, ?, ?, ?, ?)
+         """, (data["user_id"], data["age"], data["height"], data["weight"], data["gender"], now ))
+
+    conn.commit()
+    new_id =cur.lastrowid
+    conn.close()
+    return db_get_user_input(new_id)
+
+    
+
+
+
+
                     
 
 

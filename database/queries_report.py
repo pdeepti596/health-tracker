317a7cb517cd9e57ecc9c1adf28f1dcd28fe2631
Deepti <pdeepti849.com>
2026-01-16 11:46:05 +0530

    join page added

---
+++ database/queries_report.py
@@ -0,0 +1,30 @@
+
+from database.connection import get_connection
+
+def user_health_report():
+    conn = get_connection()
+    rows = conn.execute("""
+        SELECT
+            u.id AS user_id,
+            u.name,
+            u.age,
+            u.gender,
+            u.height,
+            u.weight,            
+
+            ua.steps,
+            ua.water_intake,
+            ua.calories_burned,
+
+            m.disease,
+            m.genetic_disease,
+            m.allergies
+        FROM user_inputs u
+        LEFT JOIN user_activity ua
+            ON u.id = ua.user_id
+        LEFT JOIN medical_info m
+            ON u.id = m.user_id
+    """).fetchall()
+
+    conn.close()
+    return [dict(r) for r in rows]
---
+++ services/report_service.py
@@ -0,0 +1,5 @@
+from database.queries_report import user_health_report
+
+def service_get_health_report():
+    return user_health_report()
+

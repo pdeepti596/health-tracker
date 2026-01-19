d59084944f7038ce1d3dde07a123b7e95aee8c5d
Deepti <pdeepti849.com>
2026-01-19 19:47:45 +0530

    joining page and searching done

--- database/connection.py
+++ database/connection.py
@@ -19,7 +19,7 @@ def init_database():
     # -------------------------
     conn.execute("""
         CREATE TABLE IF NOT EXISTS user_inputs (
-            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
             age INTEGER,
             gender TEXT,
